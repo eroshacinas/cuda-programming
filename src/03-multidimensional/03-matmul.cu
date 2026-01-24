4af9b78f733b564321d47339c869124f7e3f6d7f
Eros Hacinas <eros_hacinas@dlsu.edu.ph>
2026-01-24 21:46:41 +0800

    WIP: initialize matrices to non-zero values

--- src/03-multidimensional/03-matmul.cu
+++ src/03-multidimensional/03-matmul.cu
@@ -1,13 +1,75 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <cuda_runtime.h> // cuda header
+
+#define WIDTH 3 // matrix width and height
+
+// assume square block and grid 
+#define BLOCK_SIZE 4 
+#define GRID_SIZE 4
+
+// func prototypes
+void printMatrix(float M[WIDTH][WIDTH]);
+float* linearizeMatrix(float M[WIDTH][WIDTH]);
+void printLinearMatrix(float *M);
+
+__global__ void MatrixMulKernel(float *M, float *N, float *P,int Width);
+void MatrixMul(float *M_h, float *N_h, float *P_h);
 
 
 int main(){
     printf("matrix multiplication\n");
+    // initialize 2D matrix. linearize later
+    float M[WIDTH][WIDTH] = {0}, N[WIDTH][WIDTH] = {0}, P[WIDTH][WIDTH] = {0};
+
+    // printMatrix(M);
+
+    float *linearM = linearizeMatrix(M);
+    float *linearN = linearizeMatrix(N);
+    float *linearP = linearizeMatrix(P);
+
+    MatrixMul(linearM, linearN, linearP);
+
+    printLinearMatrix(linearM);
 
     return 0;
 }
 
+
+// accept linearized matrices
+void MatrixMul(
+    float *M_h,
+    float *N_h,
+    float *P_h
+) {
+    float *M_d, *N_d, *P_d;
+
+    int size = WIDTH * WIDTH * sizeof(float);
+    
+    // allocate mem in gpu
+    cudaMalloc((void**) &M_d, size);
+    cudaMalloc((void**) &N_d, size);
+    cudaMalloc((void**) &P_d, size);
+
+    // transfer to gpu
+    cudaMemcpy(M_d, M_h, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(N_d, N_h, size, cudaMemcpyHostToDevice);
+    
+    // launch kernel
+    dim3 dimGrid(GRID_SIZE, GRID_SIZE, 1);
+    dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, 1);
+
+    MatrixMulKernel<<<dimGrid, dimBlock>>>(M_d, N_d, P_d, WIDTH);
+
+    // copy result to host
+    cudaMemcpy(P_h, P_d, size, cudaMemcpyDeviceToHost);
+
+    // free mem
+    cudaFree(M_d);
+    cudaFree(N_d);
+    cudaFree(P_d);
+
+}
 
 __global__
 void MatrixMulKernel(
@@ -29,4 +91,41 @@ void MatrixMulKernel(
 
         P[row*Width+col] = Pvalue;
     }
+}
+
+
+void printMatrix(float M[WIDTH][WIDTH]) {
+    int i, j=0;
+
+    for(i=0; i<WIDTH; i++) {
+        for (j = 0; j<WIDTH; j++) {
+            printf("%f ", M[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+void printLinearMatrix(float *M){
+    int i = 0;
+
+    for(i = 0; i < WIDTH*WIDTH; i++){
+        printf("%f ", M[i]);
+
+        if((i+1) % WIDTH == 0)
+            printf("\n");
+    }
+}
+
+
+float* linearizeMatrix(float M[WIDTH][WIDTH]){
+    float *linearM = (float *) malloc(WIDTH * WIDTH * sizeof(float));
+    int i, j, index=0;
+
+    for(i=0; i<WIDTH; i++) {
+        for(j=0; j<WIDTH; j++) {
+            linearM[index++] = M[i][j];
+        }
+    }
+    return linearM;
 }
